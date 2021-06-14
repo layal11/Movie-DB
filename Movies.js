b4ff58c1d943f5892762146f312a91d2920f09b5
Layal Haidar <layal.haidar1@hotmail.com>
2021-06-14 23:11:13 +0300

    Step 6 - SEARCH

--- Movies.js
+++ Movies.js
@@ -22,6 +22,45 @@ router.get('/read' , (req , res)=>{
     res.send({status:200, data:movies });
 })
 
+router.get('/read/by-date' , (req , res)=>{
+    // router code here
+    movies.sort(function(a, b) {
+        var keyA = a.year,
+          keyB = b.year;
+        // Compare the 2 dates
+        if (keyA < keyB) return -1;
+        if (keyA > keyB) return 1;
+        return 0;
+      });
+    res.send({status:200, data: movies });
+})
+
+router.get('/read/by-rating' , (req , res)=>{
+    // router code here
+    movies.sort(function(a, b) {
+        var keyA = a.rating,
+          keyB = b.rating;
+        // Compare the 2 dates
+        if (keyA < keyB) return 1;
+        if (keyA > keyB) return -1;
+        return 0;
+      });
+    res.send({status:200, data: movies });
+})
+
+router.get('/read/by-title' , (req , res)=>{
+    // router code here
+    movies.sort(function(a, b) {
+        var keyA = a.title,
+          keyB = b.title;
+        // Compare the 2 dates
+        if (keyA < keyB) return -1;
+        if (keyA > keyB) return 1;
+        return 0;
+      });
+    res.send({status:200, data: movies });
+})
+
 router.get('/update' , (req , res)=>{
     // router code here
 })
